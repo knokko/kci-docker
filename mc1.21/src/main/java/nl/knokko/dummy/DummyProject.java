fe1855a80f2eccb2fe97e60c32a22af8a1468612
knokko <knokogator@hotmail.com>
2025-11-09 16:55:19 +0100

    Update from MC 1.21.8 to 1.21.10

--- mc1.21/src/main/java/nl/knokko/dummy/DummyProject.java
+++ mc1.21/src/main/java/nl/knokko/dummy/DummyProject.java
@@ -3,6 +3,6 @@ package nl.knokko.dummy;
 public class DummyProject {
 
     public static void main(String[] args) {
-        System.out.println(org.bukkit.craftbukkit.v1_21_R5.inventory.CraftItemStack.class.getSimpleName());
+        System.out.println(org.bukkit.craftbukkit.v1_21_R6.inventory.CraftItemStack.class.getSimpleName());
     }
 }
